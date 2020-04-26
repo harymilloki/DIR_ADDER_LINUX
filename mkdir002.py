f8af01ad8834194abccaf0aa85b906002343f423
Cloud User <lukasz@devvm01.tb2w3hrfsj4epp00wjoc4phsid.jx.internal.cloudapp.net>
2020-04-26 21:54:12 +0000

    commit9

---
+++ mkdir002.py
@@ -0,0 +1,20 @@
+import os
+
+
+class NewDirectory():
+    """docstring for ."""
+
+    def __init__(self):
+        self.name=''
+        self.permision=''
+    def add_new_dir_plus_permision(self):
+        while True:
+            self.name=raw_input('Add new directory name:')
+            print('New name for direcotry is '+self.name)
+            self.permision=raw_input('Add new permisions for directory '+self.name+':')
+            os.system('mkdir'+' '+self.name+';'+' '+'chmod'+' '+self.permision+' '+self.name+';'+' '+'ls -l'+' '+self.name)
+            break
+
+
+new_dir=NewDirectory()
+new_dir.add_new_dir_plus_permision()
